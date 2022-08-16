09bbe1a25c813eb7a57008651b1f78e56cc14bb1
Sean Orosz <seanorosz@yahoo.com>
2022-08-16 13:34:18 -0400

    properly centered all text, moved player score position, added main menu and gamestate

--- source/main.lua
+++ source/main.lua
@@ -13,6 +13,7 @@ local pd <const> = playdate
 local gfx <const> = pd.graphics
 local current, player, score, crankUI, spawnTimer, spawning
 local slimeAnim = gfx.imagetable.new("images/slime-anim")
+local gameState = 'Menu'
 
 
 math.randomseed(pd.getSecondsSinceEpoch())
@@ -20,19 +21,37 @@ math.randomseed(pd.getSecondsSinceEpoch())
 function pause()
     gfx.fillRect(0, 0, 400, 240)
     gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-    gfx.drawTextInRect('*Paused*', 165, 120, 75, 50)
+    gfx.drawTextAligned('*Paused*', 200, 120, kTextAlignment.center)
     gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
 end
 
-function restart()
-    if pd.buttonJustPressed("a") or pd.buttonJustPressed("b") then
-        gfx.sprite.removeAll()
+function clearSprites()
+    gfx.sprite.removeAll()
+    if spawnTimer then
         spawnTimer:remove()
-        initialize()
     end
 end
 
-function initialize()
+function restart()
+    if pd.buttonJustPressed("a") then
+        clearSprites()
+        startGame()
+    elseif pd.buttonJustPressed('b') then
+        gameState = 'Menu'
+        clearSprites()
+        mainMenu()
+    end
+end
+
+function mainMenu()
+    if pd.buttonJustPressed('a') then
+        gfx.clear()
+        startGame()
+    end
+end
+
+function startGame()
+    gameState = 'Game'
 
     player = Player(slimeAnim, 100, 185, 15)
     current = level(player)
@@ -52,45 +71,51 @@ function initialize()
 
 end
 
-initialize()
 
 function playdate.update()
     pd.timer.updateTimers()
     gfx.sprite.update()
-    if player.alive then
-        if not pd.isCrankDocked() then
-            if crankUI then
-                crankUI = false
+    if gameState == 'Menu' then
+        gfx.drawTextAligned('*Slime Climb*', 200, 25, kTextAlignment.center)
+        gfx.drawTextAligned('A - Start', 200, 120, kTextAlignment.center)
+        mainMenu()
+    elseif gameState == 'Game' then
+        if player.alive then
+            if not pd.isCrankDocked() then
+                if crankUI then
+                    crankUI = false
+                end
+                if not spawning then
+                    spawning = true
+                    spawnTimer:start()
+                end
+                scroll(current)
+                gfx.drawTextAligned(player.score, 200, 25, kTextAlignment.center)
+            else
+                if not crankUI then
+                    pd.ui.crankIndicator:start()
+                    crankUI = true
+                end
+                if spawning then
+                    spawning = false
+                    spawnTimer:pause()
+                end
+                pause()
+                pd.ui.crankIndicator:update()
             end
-            if not spawning then
-                spawning = true
-                spawnTimer:start()
-            end
-            scroll(current)
-            gfx.drawText(player.score, 375, 12)
         else
-            if not crankUI then
-                pd.ui.crankIndicator:start()
-                crankUI = true
-            end
             if spawning then
                 spawning = false
                 spawnTimer:pause()
             end
-            pause()
-            pd.ui.crankIndicator:update()
+            gfx.setImageDrawMode('fillWhite')
+            gfx.fillRoundRect(100, 15, 200, 125, 5)
+            gfx.drawTextAligned('Game Over', 200, 50, kTextAlignment.center)
+            gfx.drawTextAligned(player.score, 200, 25, kTextAlignment.center)
+            gfx.drawTextAligned('A - Restart', 200, 95, kTextAlignment.center)
+            gfx.drawTextAligned('B - Main Menu', 200, 115, kTextAlignment.center)
+            gfx.setImageDrawMode('fillBlack')
+            restart()
         end
-    else
-        if spawning then
-            spawning = false
-            spawnTimer:pause()
-        end
-        gfx.setImageDrawMode('fillWhite')
-        gfx.fillRoundRect(100, 15, 200, 85, 5)
-        gfx.drawText('Game Over', 161, 50)
-        gfx.drawText(player.score, 197, 25)
-        gfx.drawText('Press A or B to Restart', 110, 75)
-        gfx.setImageDrawMode('fillBlack')
-        restart()
     end
 end
